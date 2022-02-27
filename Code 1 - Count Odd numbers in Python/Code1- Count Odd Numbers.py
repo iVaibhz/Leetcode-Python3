83cd00c70e0bfb6e0d27824fde35f0094321aaa7
Vaibhav Shakkarwal <98925197+iVaibhz@users.noreply.github.com>
2022-02-27 02:13:53 -0400

    Add files via upload

---
+++ Code 1 - Count Odd numbers in Python/Code1- Count Odd Numbers.py
@@ -0,0 +1,9 @@
+class Solution:
+    
+    def countOdds(self, low: int, high: int) -> int:  
+        if low % 2 == 1 or high % 2 == 1:
+            return (high-low) // 2 + 1 
+        else:
+            return(high-low) // 2
+    
+        
